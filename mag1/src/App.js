5436f7f0cfac63bedddb89136b96ec905cb7c725
glebite <glebite@gmail.com>
2021-07-13 21:00:29 -0400

    Putting cool stuff in - playing with props and some css

--- mag1/src/App.js
+++ mag1/src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 
 import './App.css';
 import Hello from './sayHello';
+import Tweet from './tweet';
 
 function App() {
 
-    const counter = 0;
-    
   return (
     <div className="App">
           <Hello />
+
+          <Tweet name="Dev ED"/>
+          <Tweet name="Dev Ralph"/>          
     </div>
   );
 }
