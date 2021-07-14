03c3f930e28fec5cdd863dc4c0dc821188ea3639
glebite <glebite@gmail.com>
2021-07-13 20:44:50 -0400

    Adding in updates - learning about components fun wow.

--- mag1/src/App.js
+++ mag1/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
+import Hello from './sayHello';
 
 function App() {
+
+    const counter = 0;
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <Hello />
     </div>
   );
 }
